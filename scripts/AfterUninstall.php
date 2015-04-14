a218db1cf7a9764256f91f1c56645690f72e2a68
Ajay Patel <ajaypatel@atyantik.com>
2015-04-14 11:29:32 +0530

    1) Revertback the Transaction Changes 2) Added After uninstallation process script 3) Removed from Stream Module

---
+++ scripts/AfterUninstall.php
@@ -0,0 +1,14 @@
+class AfterUninstall
+{
+  public function run($conatiner)
+  {
+    $config = $conatiner->get('config');
+
+    $tabList = $config->get('tabList');
+    if (in_array('AccountIntegration', $tabList)) {
+      $config->remove('tabList');
+    }
+
+    $config->save();
+  }
+}
