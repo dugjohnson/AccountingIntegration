07ca9b48cd2605d63d195899a232427bce83d736
Ajay Patel <ajaypatel@atyantik.com>
2015-04-16 16:06:02 +0530

    Added Transaction Related Fields

--- scripts/AfterUninstall.php
+++ scripts/AfterUninstall.php
@@ -1,21 +1,22 @@
 <?php
+
 class AfterUninstall
 {
-  public function run($conatiner)
-  {
-    $config = $conatiner->get('config');
+    public function run($conatiner)
+    {
+        $config = $conatiner->get('config');
 
-    $tabList = $config->get('tabList');
-    if (in_array('Transaction', $tabList)) {
-        foreach ($tabList as $key => $value) {
-            if ($value=="Transaction") {
-                unset($tabList[$key]);
+        $tabList = $config->get('tabList');
+        if (in_array('Transaction', $tabList)) {
+            foreach ($tabList as $key => $value) {
+                if ($value == "Transaction") {
+                    unset($tabList[$key]);
+                }
             }
+            $tabList = array_values($tabList);
+            $config->set('tabList', $tabList);
         }
-        $tabList = array_values($tabList);
-      $config->set('tabList',$tabList);
-    }
 
-    $config->save();
-  }
+        $config->save();
+    }
 }
